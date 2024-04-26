58c71e4c6b2d4ee339031c2e29aaf4f17f7d3798
Cihan1990 <cihan.52@hotmail.de>
2024-04-26 11:41:02 +0200

    JPscreens

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:screens_jp/home_screen.dart';
-import 'package:screens_jp/screen_shop.dart';
+import 'package:screens_jp/src/features/home_screen/home_screen.dart';
+import 'package:screens_jp/src/features/bottom_sheet/screen_shop.dart';
 
 void main() {
   runApp(const MaterialApp(
